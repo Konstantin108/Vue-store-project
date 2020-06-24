cc6a9caf1d5dcffcddf06f9d26d66dc26f3bea76
Kostya <Kpstya@mail.ru>
2020-06-24 22:22:30 +0300

    created main.js

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        userSearch: '',
+    },
+    methods: {
+        getJson(url) {
+            return fetch(url)
+                .then(result => result.json())
+                .catch(error => {
+                    this.$refs.error.setError();
+                })
+        },
+    },
+    mounted() {
+        console.log(this);
+    }
+});
